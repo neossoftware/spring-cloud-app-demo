2d03f333fd4cc68c53ddff14e82ca90d934ca635
neossoftware <mario.hidalgo@yahoo.com.mx>
2018-11-10 11:34:56 -0600

    baseline

---
+++ limitservice-microservice/src/main/java/com/neosuniversity/micro/LimitserviceMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.neosuniversity.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitserviceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitserviceMicroserviceApplication.class, args);
+	}
+}
